00be29ed079a1ba72be6af893c60eac3322f37a7
Joshua P <65388968+montablac@users.noreply.github.com>
2022-01-30 00:08:50 +0800

    Menu Fixes and Polish

--- Menu/Menu.gd
+++ Menu/Menu.gd
@@ -9,5 +9,31 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var PollutionPercent = (GameStats.PollutionLevel / GameStats.PollutionMax) * 100
+	var ReductionAmount = 1.0
+	#if(PollutionPercent > 30):
+	ReductionAmount = 1.0 - (PollutionPercent / 30)
+	if(ReductionAmount < 0.0):
+		ReductionAmount = 0.0
+	if(ReductionAmount > 1.0):
+		ReductionAmount = 1.0
+	$Perfect.modulate.a = ReductionAmount
+	$AllGood.modulate.a = ReductionAmount
+	#if(PollutionPercent > 60):
+	ReductionAmount = 1.0 - ((PollutionPercent - 30) / 30)
+	if(ReductionAmount < 0.0):
+		ReductionAmount = 0.0
+	if(ReductionAmount > 1.0):
+		ReductionAmount = 1.0
+	$Alright.modulate.a = ReductionAmount
+	$Caution.modulate.a = ReductionAmount
+	#if(PollutionPercent > 90):
+	ReductionAmount = 1.0 - ((PollutionPercent - 60) / 30)
+	if(ReductionAmount < 0.0):
+		ReductionAmount = 0.0
+	if(ReductionAmount > 1.0):
+		ReductionAmount = 1.0
+	$Bad.modulate.a = ReductionAmount
+	$Warning.modulate.a = ReductionAmount
+	pass
