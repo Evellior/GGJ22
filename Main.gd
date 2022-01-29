af73e659edb813b682fa38b8c125f92930bba942
Joshua P <65388968+montablac@users.noreply.github.com>
2022-01-29 12:50:05 +0800

    Updated Goblins

--- Main.gd
+++ Main.gd
@@ -1,7 +1,7 @@
 extends Node
 
 export(PackedScene) var Goblin_scene
-
+var Direction = 0
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -17,11 +17,27 @@ func _ready():
 
 
 func _on_Spawn_timeout():
-	var SpawnPoint = get_node("LeftSpawn/PathFollow2D")
+	var SpawnPoint = ""
+	
+	if (Direction == 0):
+		SpawnPoint = get_node("BottomSpawn/PathFollow2D")
+	
+	if (Direction == 1):
+		SpawnPoint = get_node("LeftSpawn/PathFollow2D")
+	
+	if (Direction == 2):
+		SpawnPoint = get_node("RightSpawn/PathFollow2D")
+	
+	if (Direction == 3):
+		SpawnPoint = get_node("TopSpawn/PathFollow2D")
+	
 	SpawnPoint.offset = randi()
 	
 	var Gobo = Goblin_scene.instance()
 	add_child(Gobo)
 	
 	Gobo.position = SpawnPoint.position
-	pass # Replace with function body.
+
+
+func _on_SwapDirection_timeout():
+	Direction = randi() % 4
